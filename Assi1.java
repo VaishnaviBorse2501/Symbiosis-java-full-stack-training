642247a38a3d7be1b5ee9c1608d21d6df160937a
VaishnaviBorse2501 <borsevaishnavi291@gmail.com>
2024-09-03 13:10:46 +0530

    Add files via upload

---
+++ Assi1.java
@@ -0,0 +1,16 @@
+package demo1;
+
+public class Assi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int num1=55;
+		int num2=2;
+		int remainder1=num1-num2*(num1/num2);
+		System.out.println(remainder1);
+
+	}
+
+}
