5293c8c203df1deb9ed249dff19e2286c20286f4
VaishnaviBorse2501 <borsevaishnavi291@gmail.com>
2024-09-30 09:12:51 +0530

    Add files via upload

---
+++ assi6.java
@@ -0,0 +1,17 @@
+package assi;
+
+public class assi6 {
+	
+
+	    public static void main(String[] args) {
+
+	        char ch = 'x';
+	        int ascii = ch;
+	        // You can also cast char to int
+	        int castAscii = (int) ch;
+
+	        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+	        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+	    }
+	}
+
