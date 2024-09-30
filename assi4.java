fad7f3d805b4c37d2b3b3d87daaea12a1bf6f420
VaishnaviBorse2501 <borsevaishnavi291@gmail.com>
2024-09-30 09:11:47 +0530

    Add files via upload

---
+++ assi4.java
@@ -0,0 +1,17 @@
+package assi;
+import java.util.Scanner;
+public class assi4 {
+	
+	   public static void main(String args[]){
+	      int length, breadth, area;
+	      Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter the length of the rectangle ::");
+	      length = sc.nextInt();
+	      System.out.println("Enter the breadth of the rectangle ::");
+	      breadth = sc.nextInt();
+	      area = length* breadth;
+	      System.out.println("Area of the rectangle is ::"+area);
+	   }
+	}
+
+
