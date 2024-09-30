79023ade468074372886abaa39c5f070eb7610b7
VaishnaviBorse2501 <borsevaishnavi291@gmail.com>
2024-09-30 09:15:22 +0530

    Add files via upload

---
+++ assi9.java
@@ -0,0 +1,28 @@
+package assi;
+import java.util.Scanner;
+public class assi9 {
+	
+	    public static void main(String[] args)    
+	    {
+	        Scanner sc = new Scanner(System.in); 
+	        System.out.println("Enter a number: ");  
+	        int n = sc.nextInt();  
+	        System.out.println("The square root of "+ n+ " is: "+squareRoot(n));  
+	    }  
+	   
+	    public static double squareRoot(int num)   
+	    {  
+	        
+	        double temp;  
+	        double sqrtroot=num/2;  
+	        do   
+	        {  
+	            temp=sqrtroot;  
+	            sqrtroot=(temp+(num/temp))/2;  
+	        }   
+	        while((temp-sqrtroot)!= 0);  
+	        return sqrtroot;  
+	   }  
+	}
+
+
