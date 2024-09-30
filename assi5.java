d39d3289d9f1900044b50541c2dc48b1f2519461
VaishnaviBorse2501 <borsevaishnavi291@gmail.com>
2024-09-30 09:12:22 +0530

    Add files via upload

---
+++ assi5.java
@@ -0,0 +1,18 @@
+package assi;
+
+public class assi5 {
+	
+	  public static void main(String args[])
+	  {
+	    int radius = 3;
+	    //formula to calculate area of circle
+	    double area = Math.PI * (radius * radius);
+	    System.out.println("Area is: %.2f", area);
+
+	    //formula to calculate circumference of circle
+	    double circumference= Math.PI * 2*radius;
+	    System.out.println( "\nCircumference is: %.2f",circumference) ;
+	  }
+	}
+
+
