896681e19131215f2b54103eb3f4a037b9947f48
VaishnaviBorse2501 <borsevaishnavi291@gmail.com>
2024-09-30 09:16:38 +0530

    Add files via upload

---
+++ assi11.java
@@ -0,0 +1,29 @@
+package assi;
+
+public class assi11 {
+	//public class Student {
+	    private String name;
+	    private int score;
+	    private char grade;
+
+	    public /*Student*/assi11(String name, int score) {
+	        this.name = name;
+	        this.score = score;
+	        this.grade = calculateGrade(score);
+	    }
+
+	    private char calculateGrade(int score) {
+	        if (score >= 90) return 'A';
+	        else if (score >= 80) return 'B';
+	        else if (score >= 70) return 'C';
+	        else if (score >= 60) return 'D';
+	        else return 'F';
+	    }
+
+	    
+	    public String getName() { return name; }
+	    public int getScore() { return score; }
+	    public char getGrade() { return grade; }
+	}
+
+
