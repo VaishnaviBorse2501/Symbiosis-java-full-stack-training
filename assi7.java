54ee4a0ccea9d39a9dd2f117521fff2332de5c16
VaishnaviBorse2501 <borsevaishnavi291@gmail.com>
2024-09-30 09:14:15 +0530

    Add files via upload

---
+++ assi7.java
@@ -0,0 +1,19 @@
+package assi;
+
+public class assi7 {
+	
+
+	    public static void main(String[] args) {
+
+	        int num = 10;
+	        long factorial = 3;
+	        for(int i = 1; i <= num; ++i)
+	        {
+	            // factorial = factorial * i;
+	            factorial *= i;
+	        }
+	        System.out.printf("Factorial of %d = %d", num, factorial);
+	    }
+	}
+
+
